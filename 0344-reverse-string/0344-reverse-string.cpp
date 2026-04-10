393f1424fbcef4dd4cf76118504a39fd48504172
Muskan <154818290+muskan9567@users.noreply.github.com>
2026-04-10 10:02:59 +0530

    Time: 0 ms (100%), Space: 27 MB (97.84%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        return reverse(s.begin(),s.end());
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+  
         
     }
 };
