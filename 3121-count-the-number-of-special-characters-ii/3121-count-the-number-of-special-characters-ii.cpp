ea5fa881dbacfa9b710198cd87df9fc0a6750913
Muskan <154818290+muskan9567@users.noreply.github.com>
2026-05-27 23:10:19 +0530

    Time: 58 ms (95.51%), Space: 24.4 MB (53.09%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+        vector<int>lower(26,-1);
+        vector<int>upper(26,-1);
 
-        vector<int> lower(26, -1);
-        vector<int> upper(26, -1);
-
-        // store first occurrence
-        for(int i = 0; i < word.length(); i++) {
-
+        for(int i=0; i<word.length(); i++){
             char ch = word[i];
-
-            // lowercase
-            if(ch >= 'a' && ch <= 'z') {
-
-            
-                    lower[ch - 'a'] = i;
+            if(ch>='a' && ch<='z'){
+             
+                    lower[ch-'a']=i;
+                
                 
             }
-
-            // uppercase
-            else if(ch >= 'A' && ch <= 'Z') {
-
-                if(upper[ch - 'A'] == -1) {
-                    upper[ch - 'A'] = i;
+            else if(ch>='A' && ch<='Z'){
+                if(upper[ch-'A']==-1){
+                    upper[ch-'A']=i;
                 }
             }
         }
-
         int count = 0;
-
-        // check special characters
-        for(int i = 0; i < 26; i++) {
-
-            if(lower[i] != -1 &&
-               upper[i] != -1 &&
-               lower[i] < upper[i]) {
-
+        for(int i=0; i<26; i++){
+            if(lower[i]!=-1 && upper[i]!=-1 && lower[i] < upper[i]){
                 count++;
             }
         }
-
+  
         return count;
+        
     }
 };
