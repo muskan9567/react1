40e40b2506eef4b974b83dc19f8abbcff0fabe05
Muskan <154818290+muskan9567@users.noreply.github.com>
2026-06-10 10:51:25 +0530

    Time: 14 ms (27.87%), Space: 23.3 MB (11.99%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+
+    vector<int> nextsmallerright(vector<int>& matrix, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+        for(int i = n - 1; i >= 0; i--) {
+            int curr = matrix[i];
+            while(s.top() != -1 && matrix[s.top()] >= curr) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    vector<int> nextsmallerleft(vector<int>& matrix, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++) {
+            int curr = matrix[i];
+            while(s.top() != -1 && matrix[s.top()] >= curr) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    int findarea(vector<int>& matrix, int n) {
+        vector<int> NSR = nextsmallerright(matrix, n);
+        vector<int> NSL = nextsmallerleft(matrix, n);
+        int area = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            int length = matrix[i];
+            if(NSR[i] == -1) {
+                NSR[i] = n;
+            }
+            int width = NSR[i] - NSL[i] - 1;
+            int newarea = length * width;
+            area = max(area, newarea);
+        }
+        return area;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> temp(m, 0);
+
+        for(int j = 0; j < m; j++) {
+            temp[j] = matrix[0][j] - '0';
+        }
+
+        int area = findarea(temp, m);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == '1') {
+                    temp[j] = temp[j] + 1;
+                }
+                else {
+                    temp[j] = 0;
+                }
+            }
+            area = max(area, findarea(temp, m));
+        }
+        return area;
+    }
+};
